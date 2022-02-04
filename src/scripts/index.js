f3c2d6c777803d53839f39ab805d93b6d9d62cdf
LonelyGuyPiny <lonelyguypiny@gmail.com>
2022-02-03 23:33:12 -0500

    Finish the mouse movement

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import '../styles/style.css';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 import { earth, earthAtmosphere } from './earth';
 import { stars } from './stars';
@@ -16,6 +17,20 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+
+// add the objects to the scene
+const singleEarthGroup = new THREE.Group();
+singleEarthGroup.add(earth);
+singleEarthGroup.rotation.offset = {
+    x: 0,
+    y: 0
+};
+scene.add(singleEarthGroup);
+scene.add(earthAtmosphere);
+earthAtmosphere.scale.set(1.1, 1.1, 1.1);
+scene.add(stars);
+
+// add event listeners
 window.addEventListener('resize', () => {
     // update camera
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -26,17 +41,54 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// add the objects to the scene
-scene.add(earth);
-scene.add(earthAtmosphere);
-earthAtmosphere.scale.set(1.1, 1.1, 1.1);
-scene.add(stars);
+const mouse = {
+    down: false,
+    x: undefined,
+    y: undefined,
+    prevX: undefined,
+    prevY: undefined
+};
+canvas.addEventListener('mousedown', (event) => {
+    if (event.button === 0) { // left mouse button
+        mouse.down = true;
+        mouse.prevX = event.clientX;
+        mouse.prevY = event.clientY;
+    }
+});
+canvas.addEventListener('mousemove', (event) => {
+    mouse.x = ((event.clientX - innerWidth / 2) / (innerWidth / 2)) * 2 - 1;
+    mouse.y = -(event.clientY / innerHeight) * 2 + 1;
 
+    if (mouse.down) {
+        event.preventDefault();
+        // console.log('turn the earth')
+        const deltaX = event.clientX - mouse.prevX;
+        const deltaY = event.clientY - mouse.prevY;
+
+        singleEarthGroup.rotation.offset.x += deltaY * 0.005;
+        singleEarthGroup.rotation.offset.y += deltaX * 0.005;
+
+        gsap.to(singleEarthGroup.rotation, {
+            y: singleEarthGroup.rotation.offset.y,
+            x: singleEarthGroup.rotation.offset.x,
+            duration: 2
+        })
+        mouse.prevX = event.clientX;
+        mouse.prevY = event.clientY;
+    }
+});
+canvas.addEventListener('mouseup', (event) => {
+    if (event.button === 0) { // left mouse button
+        mouse.down = false;
+    }
+});
+
+// set the camera z-position
 camera.position.z = 10;
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    earth.rotation.y += 0.001;
+    // mouse event animation
 }
 animate();
